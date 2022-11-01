d1c1e4d9c3d9daf0818e5bb5510ac8edd4d2edb0
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-01 12:30:19 +0100

    Configuration
    - Added package Microsoft.Extensions.Options.ConfigurationExtensions
    - Introduced type FindNationalityOptions
    - Updated Function class to customize configuration system
    - Updated handler to receive and use incoming options

--- FindNationalityFunction/Function.cs
+++ FindNationalityFunction/Function.cs
@@ -5,6 +5,7 @@ using Kralizek.Lambda;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -18,6 +19,10 @@ public class Function : RequestResponseFunction<string, Country[]>
 
     // Add registration for HttpClient
     services.AddHttpClient("Nationality");
+
+    services.AddOptions();
+
+    services.Configure<FindNationalityOptions>(Configuration.GetSection("Options"));
   }
 
   protected override void ConfigureLogging(ILoggingBuilder logging, IExecutionEnvironment executionEnvironment)
@@ -34,29 +39,38 @@ public class Function : RequestResponseFunction<string, Country[]>
       logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
     }
   }
-}
 
+  protected override void Configure(IConfigurationBuilder builder)
+  {
+    builder.AddInMemoryCollection(new Dictionary<string, string>
+    {
+      ["Options:MinimumThreshold"] = "0.05"
+    });
+  }
+}
 
 public class FindNationalityHandler : IRequestResponseHandler<string, Country[]>
 {
   private readonly IHttpClientFactory _httpClientFactory;
   private readonly ILogger<FindNationalityHandler> _logger;
+  private readonly FindNationalityOptions _options;
 
-  public FindNationalityHandler(IHttpClientFactory httpClientFactory, ILogger<FindNationalityHandler> logger)
+  public FindNationalityHandler(IHttpClientFactory httpClientFactory, IOptions<FindNationalityOptions> options, ILogger<FindNationalityHandler> logger)
   {
     _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+    _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
   }
 
   public async Task<Country[]> HandleAsync(string? input, ILambdaContext context)
   {
-    var http = _httpClientFactory.CreateClient("Nationality");
+    var http = _httpClientFactory.CreateClient(nameof(FindNationalityHandler));
 
     _logger.LogDebug("Finding the nationality of people named {Name}", input);
 
     var response = await http.GetFromJsonAsync<Response>($"https://api.nationalize.io/?name={input}");
 
-    return response?.Countries ?? Array.Empty<Country>();
+    return response?.Countries.Where(c => c.Probability >= _options.MinimumThreshold).ToArray() ?? Array.Empty<Country>();
   }
 }
 
@@ -69,3 +83,8 @@ public record Country(
     [property: JsonPropertyName("country_id")] string CountryCode,
     [property: JsonPropertyName("probability")] double Probability
 );
+
+public record FindNationalityOptions
+{
+  public double MinimumThreshold { get; init; }
+}
