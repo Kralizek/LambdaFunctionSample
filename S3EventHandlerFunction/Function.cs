15cb9911bf43b41315ec76e3bba7f5101b2092e5
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-04 17:31:11 +0100

    Wired up S3 event handler

--- S3EventHandlerFunction/Function.cs
+++ S3EventHandlerFunction/Function.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.SQS;
 using Kralizek.Lambda;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,16 +16,20 @@ public class Function : EventFunction<S3Event>
 {
     protected override void Configure(IConfigurationBuilder builder)
     {
-        // Use this method to register your configuration flow. Exactly like in ASP.NET Core
-    }
-
-    protected override void ConfigureLogging(ILoggingBuilder logging, IExecutionEnvironment executionEnvironment)
-    {
-        // Use this method to configure the logging
+        builder.AddInMemoryCollection(new Dictionary<string, string>
+        {
+            ["Options:QueueUrl"] = "url to my SQS queue"
+        });
     }
 
     protected override void ConfigureServices(IServiceCollection services, IExecutionEnvironment executionEnvironment)
     {
         RegisterHandler<S3EventHandler>(services);
+
+        services.AddAWSService<IAmazonS3>();
+
+        services.AddAWSService<IAmazonSQS>();
+
+        services.Configure<S3EventHandlerOptions>(Configuration.GetSection("Options"));
     }
 }
