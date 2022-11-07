44e696f4d004ef4588bd231bdd502a03b72f548a
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-07 16:57:17 +0100

    Wired up SQS message handler

--- SqsEventHandlerFunction/Function.cs
+++ SqsEventHandlerFunction/Function.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using Amazon.SimpleNotificationService;
 using Kralizek.Lambda;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,8 +13,24 @@ namespace SqsEventHandlerFunction;
 
 public class Function : EventFunction<SQSEvent>
 {
+    protected override void Configure(IConfigurationBuilder builder)
+    {
+        builder.AddInMemoryCollection(new Dictionary<string, string>
+        {
+            ["Options:TopicArn"] = "ARN of my SNS topic"
+        });
+    }
+
     protected override void ConfigureServices(IServiceCollection services, IExecutionEnvironment executionEnvironment)
     {
-        services.UseQueueMessageHandler<Entry, SqsMessageHandler>();
+        services.UseQueueMessageHandler<Entry, SqsMessageHandler>().WithParallelExecution();
+
+        services.AddAWSService<IAmazonSimpleNotificationService>();
+
+        services.Configure<SqsMessageHandlerOptions>(Configuration.GetSection("Options"));
+
+        services.UseCustomMessageSerializer<CustomSerializer>();
+
+        services.AddSingleton<IItemProcessor, NullItemProcessor>();
     }
 }
